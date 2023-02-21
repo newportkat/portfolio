94c9c3acb3d4c9793145ae877b1f52958feb81cb
newportkat <thomas.katilius@gmail.com>
2023-02-21 13:31:21 +1100

    added new project

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import PalettePro from "./assets/palettepro.png"
+import TackleShop from "./assets/tackleshop.png"
 
 const App = () => {
     const [darkMode, setDarkMode] = useState(true)
@@ -120,7 +121,7 @@ const App = () => {
                 <div className="flex flex-col gap-8 sm:flex-row sm:gap-36">
                     <div className="flex flex-col gap-2">
                         <h2 className="text-xl tracking-wide text-blue-400">
-                            I'm Familiar Working With:
+                            I'm Love Working With:
                         </h2>
                         <div className="flex gap-8 tracking-wide">
                             <ul className="leading-relaxed">
@@ -139,21 +140,6 @@ const App = () => {
                             </ul>
                         </div>
                     </div>
-                    <div className="flex flex-col gap-2">
-                        <h2 className="text-xl tracking-wide text-blue-400">
-                            I've Experimented With:
-                        </h2>
-                        <div className="flex gap-8 tracking-wide">
-                            <ul className="leading-relaxed">
-                                <li>- Python</li>
-                                <li>- Nginx</li>
-                            </ul>
-                            <ul>
-                                <li>- Strapi</li>
-                                <li>- Redux</li>
-                            </ul>
-                        </div>
-                    </div>
                 </div>
             </section>
             <section className="flex flex-col items-center justify-center gap-8 bg-blue-200 px-8 py-16 text-center text-gray-700 dark:bg-blue-900 dark:text-gray-200 md:gap-12 md:px-12">
@@ -163,6 +149,101 @@ const App = () => {
                 >
                     PROJECTS
                 </h1>
+                <div className="flex flex-col items-center gap-8 md:flex-row md:gap-16">
+                    <div className="group overflow-hidden rounded-lg md:w-1/2">
+                        <a href="http://shop.tomkatilius.com/" target="_blank">
+                            <img
+                                src={TackleShop}
+                                alt=""
+                                className="h-full w-full transform rounded object-cover opacity-90 transition-all duration-1000 ease-in-out group-hover:scale-125 group-hover:opacity-100"
+                            />
+                        </a>
+                    </div>
+                    <div className="flex flex-col gap-6 md:w-1/2">
+                        <h2 className="text-2xl font-medium uppercase tracking-wider md:text-left">
+                            Tackle Shop
+                        </h2>
+                        <div className="flex flex-wrap items-center justify-center gap-6 md:justify-start">
+                            <a
+                                href="https://github.com/newportkat/e-commerce"
+                                target="_blank"
+                                className="flex items-center gap-2 rounded bg-gray-600 p-2 font-medium tracking-wider text-white hover:bg-gray-700 active:bg-gray-600"
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth={2}
+                                    stroke="currentColor"
+                                    className="h-6 w-6"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5"
+                                    />
+                                </svg>
+                                <p>GitHub Repo</p>
+                            </a>
+                            <a
+                                href="http://shop.tomkatilius.com/"
+                                target="_blank"
+                                className="flex items-center gap-2 rounded bg-gray-600 p-2 font-medium tracking-wider text-white hover:bg-gray-700 active:bg-gray-600"
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth={2}
+                                    stroke="currentColor"
+                                    className="h-6 w-6"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                                    />
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                                    />
+                                </svg>
+                                <p>Live Demo</p>
+                            </a>
+                        </div>
+                        <div className="flex flex-col gap-6 text-left leading-8 tracking-wide">
+                            <p>
+                                Welcome to my mock e-commerce store! This
+                                project is a full-stack web application built
+                                using{" "}
+                                <span className="font-bold">
+                                    React, TypeScript, and TailwindCSS
+                                </span>{" "}
+                                for the front-end, and a{" "}
+                                <span className="font-bold">
+                                    Node.js Express server
+                                </span>{" "}
+                                for the back-end. The app is hosted on a{" "}
+                                <span className="font-bold">
+                                    VPS with Nginx.
+                                </span>{" "}
+                            </p>
+                            <p>
+                                The front-end makes use of the{" "}
+                                <span className="font-bold">
+                                    Context API and the useReducer hook
+                                </span>{" "}
+                                to manage the state of the cart. The UI design
+                                is implemented using the TailwindCSS framework,
+                                providing a modern and responsive user
+                                interface. All of the mock product data is
+                                served to the client through a{" "}
+                                <span className="font-bold">REST API.</span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
                 <div className="flex flex-col items-center gap-8 md:flex-row md:gap-16">
                     <div className="group overflow-hidden rounded-lg md:w-1/2">
                         <a href="https://palettepro.digital/" target="_blank">
@@ -226,18 +307,21 @@ const App = () => {
                                 <p>Live Demo</p>
                             </a>
                         </div>
-                        <p className="leading-8 tracking-wide md:text-left">
+                        <p className="text-left leading-8 tracking-wide">
                             Palette Pro is a color palette generator built with
                             <span className="font-bold">
                                 {" "}
-                                React, TypeScript, and Tailwind.
+                                React, TypeScript, and TailwindCSS.
                             </span>{" "}
                             It features the ability to generate palettes from a
                             range of color modes, adjust individual HSL values,
                             lock colors and save palettes to local storage. An
-                            integrated binary search algorithm provides the
-                            closest color name match from a database of over
-                            3000 names.
+                            integrated{" "}
+                            <span className="font-bold">
+                                binary search algorithm
+                            </span>{" "}
+                            provides the closest color name match from a
+                            database of over 3000 names.
                         </p>
                     </div>
                 </div>
